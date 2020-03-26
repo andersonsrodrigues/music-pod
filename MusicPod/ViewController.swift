181541cba165dfede7952a81d3db4d82c52b70da
andersonsrodrigues <srodrigues.anderson@gmail.com>
2020-03-26 15:02:49 -0300

    Initial Commit

---
+++ MusicPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPod
+//
+//  Created by Anderson Soares Rodrigues on 26/03/20.
+//  Copyright © 2020 Anderson Soares Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
